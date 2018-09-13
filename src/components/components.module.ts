3c29dafe5c7fe7067489a28de5ddb0b4a79dba7b
nircoh <99nirc99@gmail.com>
2018-09-13 12:56:58 +0300

    lots of things

--- src/components/components.module.ts
+++ src/components/components.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { FacilityComponent } from './facility/facility';
 import {IonicModule} from "ionic-angular";
+import { FacilitiesListComponent } from './facilities-list/facilities-list';
+import {FacilitiesProvider} from "../providers/facilities/facilities";
 import {TransportsManagerComponent} from "./transports-manager/transports-manager";
 import {TransportComponent} from "./transport/transport";
 @NgModule({
-	imports: [IonicModule],
 	declarations: [FacilityComponent,
+    FacilitiesListComponent,
     TransportsManagerComponent,
     TransportComponent],
+	imports: [IonicModule],
+  providers: [FacilitiesProvider],
 	exports: [FacilityComponent,
+    FacilitiesListComponent,
     TransportsManagerComponent,
     TransportComponent]
 })
