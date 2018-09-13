7731a6a6ad131d072723a2b8ed7d043946bbbcfd
nitzanmo <nitzanmousan@gmail.com>
2018-09-13 11:56:12 +0300

    Added facility component

---
+++ src/components/components.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { FacilityComponent } from './facility/facility';
+@NgModule({
+	declarations: [FacilityComponent],
+	imports: [],
+	exports: [FacilityComponent]
+})
+export class ComponentsModule {}
